869c6fcbb627792818900ca2ac63a081f9ca7617
SrLampi1001 <sanchezsantiago1001@gmail.com>
2026-01-20 16:05:59 -0500

    added user & admin models abstract

---
+++ models/admin.js
@@ -0,0 +1,13 @@
+import User from "./user.js";
+export default class Admin extends User{
+    rol = "admin";
+    constructor(id, username, email, number, age){
+        super(id, username, email, number, age);
+    }
+    
+    createAdmin(User){
+        //Update method to include the deletion of the prevous user, and update the JSON file accordingly
+        return new Admin(User.id, User.username, User.email, User.number, User.age);
+    }
+    
+}
