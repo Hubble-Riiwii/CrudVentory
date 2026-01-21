a9c5208c57fa0fd90e8568fa07ff364ec2f1e4ad
SrLampi1001 <sanchezsantiago1001@gmail.com>
2026-01-20 20:37:41 -0500

    update: models & utils for login & signup

---
+++ Back/utils/sign_in.js
@@ -0,0 +1,26 @@
+import User from "../models/User.js";
+export const signIn= async (name, password, email)=>{
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/; //Regex to verify if it's username or email
+    if(!emailRegex.test(user)){
+        return new Error("Email invalid")
+    }
+    try{
+        const user = {
+            name: name,
+            email:email,
+            password:password
+        }
+        const response = await fetch(User.db+"users/", {
+            method:"POST",
+            headers:{"Content-Type":"applicaton/json"},
+            body:JSON.stringify(user)
+        })
+        if(!response.ok){
+            return new Error(`HTTP ERROR!, ${response.status}`)
+        }
+        const data = await response.json();
+        return new User(data?.id, data?.name, data?.email, data?.password)
+    }catch(er){
+        console.error("Error", er)
+    }
+}
