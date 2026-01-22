9c7bc7b174a9b365a2108e81c1071033af5e0cd1
SrLampi1001 <sanchezsantiago1001@gmail.com>
2026-01-22 15:16:29 -0500

    Fix: Product Static methods using this
    Aditionaly a few verifications for getProduct method and UpdateProduct

--- Back/models/Product.js
+++ Back/models/Product.js
@@ -43,29 +43,35 @@ export default class Product {
     }
 
     static async getProductById(idProduct) {
+        const product = Product.products.get(idProduct) ?? null; //Simple verification for product inside memory
+        if(product !== null){
+            return product
+        }
         try {
-            const response = await fetch(this.db + "/products/" + idProduct, {
+            const response = await fetch(Product.db + "/products/:" + idProduct, {
                 method: "GET",
                 headers: { "Content-Type": "application/json" }
             })
             if (!response.ok) {
                 return new Error(`HTTP Error! ${response.status}`);
             }
-            data = await response.json();
-
-            return data.map(p => new Product(p?.id, p?.name, p?.category_id, p?.price, p?.stock, p?.description, p?.images, p?.product_state));
+            p = await response.json();
+            const product = new Product(p?.id, p?.name, p?.category_id, p?.price, p?.stock, p?.description, p?.images, p?.product_state);
+            Product.products.set(product.id, product)
+            return product;
 
         } catch (error) {
             console.error("error", error)
+            return null
         }
     }
 
-    static async createProduct(Product) {
+    static async createProduct(Prod) {
         try {
-            const response = await fetch(this.db + "/products/", {
+            const response = await fetch(Product.db + "/products/", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(Product)
+                body: JSON.stringify(Prod)
             })
             if (!response.ok) {
                 return new Error(`HTTP ERROR!, ${response.status}`)
@@ -79,12 +85,15 @@ export default class Product {
         }
     }
 
-    static async editProductById(Product, idProduct) {
+    static async editProductById(Prod, idProduct) {
+        if(Prod.id !== idProduct){      //Simple verification for id
+            return new Error("ERROR! Not the same id")  
+        }
         try {
-            const response = await fetch(this.db + "/products/" + idProduct, {
+            const response = await fetch(Product.db + "/products/:" + idProduct, {
                 method: "PUT",
                 headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(Product)
+                body: JSON.stringify(Prod)
             })
             if (!response.ok) {
                 return new Error(`HTTP ERROR!, ${response.status}`)
@@ -100,7 +109,7 @@ export default class Product {
 
     static async deleteProductById(idProduct) {
         try {
-            const response = await fetch(this.db + "/products/" + idProduct, {
+            const response = await fetch(Product.db + "/products/" + idProduct, {
                 method: "DELETE",
                 headers: { "Content-Type": "application/json" },
             })
