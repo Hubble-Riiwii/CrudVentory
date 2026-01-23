9e46be2e0915022bec08801fb0c2e2d0fabe9776
SrLampi1001 <sanchezsantiago1001@gmail.com>
2026-01-22 20:44:45 -0500

    fix: admin & user addTocart()

--- Back/models/Admin.js
+++ Back/models/Admin.js
@@ -1,12 +1,14 @@
 import User from "./User.js";
 export default class Admin extends User{
     role = "admin";
-    constructor(id, username, email, password){
-        super(id, username, email, password);
+    constructor(id, name, email, password){
+        super(id, name, email, password);
     }
     createAdmin(User){
         //Update method to include the deletion of the prevous user, and update the JSON file accordingly
-        return new Admin(User.id, User.username, User.email, User.number, User.age);
+        return new Admin(User.id, User.name, User.email, User.password);
     }
-    
+    updateProduct(productId, {}){
+        //update product
+    }
 }
--- Back/models/User.js
+++ Back/models/User.js
@@ -3,20 +3,25 @@ export default class User{
     static db = "http://localhost:3000";
     static isListening = false;
     role = "user";
-    constructor(id, name, email, password){
+    constructor(id, name, email, password, cart = new Map()){
         this.id=id;
         this.name=name;
         this.email=email;
         this.password = password;
-        User.initializeEventListeners();
+        this.cart = cart; //Expected to include in constructor
     }
     //Add methods for buying and updating user information
-    static initializeEventListeners(){
-        if(User.isListening){
-            return;
-        }
+    async buyProduct(productId, amount){
+        const product = await Product.getProductById(productId);
+        if(product===null) return;
+        //product.sell(amount)    //Expected method to reduce the product amount database 
     }
-    async buyProduct(){
-        const product = await Product.getProductById(idProduct);
+    
+    async addProductToCart(productId, amount=1){
+        amountInCart = this.cart.get(productId)
+        if(amountInCart!==null){
+            this.cart.set(productId, amountInCart+amount);
+        }
+        this.cart.set(productId, amount)
     }
 }
