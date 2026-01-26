45d41239d8f0cc729d8e48f453371478c8f75020
SrLampi1001 <sanchezsantiago1001@gmail.com>
2026-01-26 08:17:23 -0500

    update: name & logout button dashboard

--- Front/admin/js/admin.script.js
+++ Front/admin/js/admin.script.js
@@ -3,7 +3,17 @@ document.addEventListener("DOMContentLoaded", ()=>{
     const admin = JSON.parse(sessionStorage.getItem("user"))
     if(admin === null)location = "../"; //return to index if not user in sesionStorage
     if(admin.role !=="admin")location = "../"; //return to index if user is not admin
-})
+});
+(()=>{
+    const options = document.querySelector(".iconos-opciones");
+    document.querySelector(".admin-name").textContent = JSON.parse(sessionStorage.getItem("user")).name;
+    const LogoutBtn = document.createElement("a");
+    LogoutBtn.textContent = "Logout"; LogoutBtn.href = "../"; LogoutBtn.classList.add("geneva");
+    LogoutBtn.addEventListener("click", ()=>{
+        sessionStorage.removeItem("user");
+    });
+    options.appendChild(LogoutBtn);
+})()
 const admin = Admin.createAdmin(JSON.parse(sessionStorage.getItem("user")))
 await Admin.fetchUsers();
 console.log(Admin.users)
